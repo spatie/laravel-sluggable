61d22123c7bd0c9a13e9da62368b5204d21e804b
Carlos Eduardo <juniob2ss@gmail.com>
2016-06-22 23:40:52 -0300

    Remove PHP 7.0 require

--- src/HasSlug.php
+++ src/HasSlug.php
@@ -4,133 +4,124 @@ namespace Spatie\Sluggable;
 
 use Illuminate\Database\Eloquent\Model;
 
-trait HasSlug
-{
-    /** @var \Spatie\Sluggable\SlugOptions */
-    protected $slugOptions;
+trait HasSlug {
+	/** @var \Spatie\Sluggable\SlugOptions */
+	protected $slugOptions;
 
-    /**
-     * Get the options for generating the slug.
-     */
-    abstract public function getSlugOptions(): SlugOptions;
+	/**
+	 * Get the options for generating the slug.
+	 */
+	abstract public function getSlugOptions();
 
-    /**
-     * Boot the trait.
-     */
-    protected static function bootHasSlug()
-    {
-        static::creating(function (Model $model) {
-            $model->addSlug();
-        });
+	/**
+	 * Boot the trait.
+	 */
+	protected static function bootHasSlug() {
+		static::creating(function (Model $model) {
+			$model->addSlug();
+		});
 
-        static::updating(function (Model $model) {
-            $model->addSlug();
-        });
-    }
+		static::updating(function (Model $model) {
+			$model->addSlug();
+		});
+	}
 
-    /**
-     * Add the slug to the model.
-     */
-    protected function addSlug()
-    {
-        $this->slugOptions = $this->getSlugOptions();
+	/**
+	 * Add the slug to the model.
+	 */
+	protected function addSlug() {
+		$this->slugOptions = $this->getSlugOptions();
 
-        $this->guardAgainstInvalidSlugOptions();
+		$this->guardAgainstInvalidSlugOptions();
 
-        $slug = $this->generateNonUniqueSlug();
+		$slug = $this->generateNonUniqueSlug();
 
-        if ($this->slugOptions->generateUniqueSlugs) {
-            $slug = $this->makeSlugUnique($slug);
-        }
+		if ($this->slugOptions->generateUniqueSlugs) {
+			$slug = $this->makeSlugUnique($slug);
+		}
 
-        $slugField = $this->slugOptions->slugField;
+		$slugField = $this->slugOptions->slugField;
 
-        $this->$slugField = $slug;
-    }
+		$this->$slugField = $slug;
+	}
 
-    /**
-     * Generate a non unique slug for this record.
-     */
-    protected function generateNonUniqueSlug(): string
-    {
-        if ($this->hasCustomSlugBeenUsed()) {
-            $slugField = $this->slugOptions->slugField;
+	/**
+	 * Generate a non unique slug for this record.
+	 */
+	protected function generateNonUniqueSlug() {
+		if ($this->hasCustomSlugBeenUsed()) {
+			$slugField = $this->slugOptions->slugField;
 
-            return $this->$slugField;
-        }
+			return $this->$slugField;
+		}
 
-        return str_slug($this->getSlugSourceString());
-    }
+		return str_slug($this->getSlugSourceString());
+	}
 
-    /**
-     * Determine if a custom slug has been saved.
-     */
-    protected function hasCustomSlugBeenUsed(): bool
-    {
-        $slugField = $this->slugOptions->slugField;
+	/**
+	 * Determine if a custom slug has been saved.
+	 */
+	protected function hasCustomSlugBeenUsed() {
+		$slugField = $this->slugOptions->slugField;
 
-        return $this->getOriginal($slugField) != $this->$slugField;
-    }
+		return $this->getOriginal($slugField) != $this->$slugField;
+	}
 
-    /**
-     * Get the string that should be used as base for the slug.
-     */
-    protected function getSlugSourceString(): string
-    {
-        if (is_callable($this->slugOptions->generateSlugFrom)) {
-            $slugSourceString = call_user_func($this->slugOptions->generateSlugFrom, $this);
-            return substr($slugSourceString, 0, $this->slugOptions->maximumLength);
-        }
+	/**
+	 * Get the string that should be used as base for the slug.
+	 */
+	protected function getSlugSourceString() {
+		if (is_callable($this->slugOptions->generateSlugFrom)) {
+			$slugSourceString = call_user_func($this->slugOptions->generateSlugFrom, $this);
+			return substr($slugSourceString, 0, $this->slugOptions->maximumLength);
+		}
 
-        $slugSourceString = collect($this->slugOptions->generateSlugFrom)
-            ->map(function (string $fieldName) : string {
-                return $this->$fieldName ?? '';
-            })
-            ->implode('-');
+		$slugSourceString = collect($this->slugOptions->generateSlugFrom)
+			->map(function (string $fieldName) {
+				return $this->$fieldName ?? '';
+			})
+			->implode('-');
 
-        return substr($slugSourceString, 0, $this->slugOptions->maximumLength);
-    }
+		return substr($slugSourceString, 0, $this->slugOptions->maximumLength);
+	}
 
-    /**
-     * Make the given slug unique.
-     */
-    protected function makeSlugUnique(string $slug): string
-    {
-        $originalSlug = $slug;
-        $i = 1;
+	/**
+	 * Make the given slug unique.
+	 */
+	protected function makeSlugUnique(string $slug) {
+		$originalSlug = $slug;
+		$i = 1;
 
-        while ($this->otherRecordExistsWithSlug($slug) || $slug === '') {
-            $slug = $originalSlug.'-'.$i++;
-        }
+		while ($this->otherRecordExistsWithSlug($slug) || $slug === '') {
+			$slug = $originalSlug . '-' . $i++;
+		}
 
-        return $slug;
-    }
+		return $slug;
+	}
 
-    /**
-     * Determine if a record exists with the given slug.
-     */
-    protected function otherRecordExistsWithSlug(string $slug): bool
-    {
-        return (bool) static::where($this->slugOptions->slugField, $slug)
-            ->where($this->getKeyName(), '!=', $this->getKey() ?? '0')
-            ->first();
-    }
+	/**
+	 * Determine if a record exists with the given slug.
+	 */
+	protected function otherRecordExistsWithSlug(string $slug) {
+		return (bool) static::where($this->slugOptions->slugField, $slug)
+			->where($this->getKeyName(), '!=', $this->getKey() ?? '0')
+			->first();
+	}
 
-    /**
-     * This function will throw an exception when any of the options is missing or invalid.
-     */
-    protected function guardAgainstInvalidSlugOptions()
-    {
-        if (!count($this->slugOptions->generateSlugFrom)) {
-            throw InvalidOption::missingFromField();
-        }
+	/**
+	 * This function will throw an exception when any of the options is missing or invalid.
+	 */
+	protected function guardAgainstInvalidSlugOptions() {
+		if (!count($this->slugOptions->generateSlugFrom)) {
+			throw InvalidOption::missingFromField();
+		}
 
-        if (!strlen($this->slugOptions->slugField)) {
-            throw InvalidOption::missingSlugField();
-        }
+		if (!strlen($this->slugOptions->slugField)) {
+			throw InvalidOption::missingSlugField();
+		}
 
-        if ($this->slugOptions->maximumLength <= 0) {
-            throw InvalidOption::invalidMaximumLength();
-        }
-    }
+		if ($this->slugOptions->maximumLength <= 0) {
+			throw InvalidOption::invalidMaximumLength();
+		}
+	}
 }
--- tests/Integration/HasSlugTest.php
+++ tests/Integration/HasSlugTest.php
@@ -2,184 +2,159 @@
 
 namespace Spatie\Sluggable\Test\Integration;
 
-use Spatie\Sluggable\SlugOptions;
+class HasSlugTest extends TestCase {
+	/** @test */
+	public function itWillSaveASlugWhenSavingAModel() {
+		$model = TestModel::create(['name' => 'this is a test']);
 
-class HasSlugTest extends TestCase
-{
-    /** @test */
-    public function it_will_save_a_slug_when_saving_a_model()
-    {
-        $model = TestModel::create(['name' => 'this is a test']);
+		$this->assertEquals('this-is-a-test', $model->url);
+	}
 
-        $this->assertEquals('this-is-a-test', $model->url);
-    }
+	/** @test */
+	public function itCanHandleNullValuesWhenCreatingSlugs() {
+		$model = TestModel::create(['name' => null]);
 
-    /** @test */
-    public function it_can_handle_null_values_when_creating_slugs()
-    {
-        $model = TestModel::create(['name' => null]);
+		$this->assertEquals('-1', $model->url);
+	}
 
-        $this->assertEquals('-1', $model->url);
-    }
+	/** @test */
+	public function itWillNotChangeTheSlugWhenTheSourceFieldIsNotChanged() {
+		$model = TestModel::create(['name' => 'this is a test']);
 
-    /** @test */
-    public function it_will_not_change_the_slug_when_the_source_field_is_not_changed()
-    {
-        $model = TestModel::create(['name' => 'this is a test']);
+		$model->other_field = 'otherValue';
+		$model->save();
 
-        $model->other_field = 'otherValue';
-        $model->save();
+		$this->assertEquals('this-is-a-test', $model->url);
+	}
 
-        $this->assertEquals('this-is-a-test', $model->url);
-    }
+	/** @test */
+	public function itWillUpdateTheSlugWhenTheSourceFieldIsChanged() {
+		$model = TestModel::create(['name' => 'this is a test']);
 
-    /** @test */
-    public function it_will_update_the_slug_when_the_source_field_is_changed()
-    {
-        $model = TestModel::create(['name' => 'this is a test']);
+		$model->name = 'this is another test';
+		$model->save();
 
-        $model->name = 'this is another test';
-        $model->save();
+		$this->assertEquals('this-is-another-test', $model->url);
+	}
 
-        $this->assertEquals('this-is-another-test', $model->url);
-    }
+	/** @test */
+	public function itWillSaveAUniqueSlugByDefault() {
+		TestModel::create(['name' => 'this is a test']);
 
-    /** @test */
-    public function it_will_save_a_unique_slug_by_default()
-    {
-        TestModel::create(['name' => 'this is a test']);
+		foreach (range(1, 10) as $i) {
+			$model = TestModel::create(['name' => 'this is a test']);
+			$this->assertEquals("this-is-a-test-{$i}", $model->url);
+		}
+	}
 
-        foreach (range(1, 10) as $i) {
-            $model = TestModel::create(['name' => 'this is a test']);
-            $this->assertEquals("this-is-a-test-{$i}", $model->url);
-        }
-    }
+	/** @test */
+	public function itCanHandleEmptySourceFields() {
+		foreach (range(1, 10) as $i) {
+			$model = TestModel::create(['name' => '']);
+			$this->assertEquals("-{$i}", $model->url);
+		}
+	}
 
-    /** @test */
-    public function it_can_handle_empty_source_fields()
-    {
-        foreach (range(1, 10) as $i) {
-            $model = TestModel::create(['name' => '']);
-            $this->assertEquals("-{$i}", $model->url);
-        }
-    }
+	/** @test */
+	public function itCanGenerateSlugsFromMultipleSourceFields() {
+		$model = new class extends TestModel {
+			public function getSlugOptions() {
+				return parent::getSlugOptions()->generateSlugsFrom(['name', 'other_field']);
+			}
+		};
 
-    /** @test */
-    public function it_can_generate_slugs_from_multiple_source_fields()
-    {
-        $model = new class extends TestModel
-        {
-            public function getSlugOptions(): SlugOptions
-            {
-                return parent::getSlugOptions()->generateSlugsFrom(['name', 'other_field']);
-            }
-        };
+		$model->name = 'this is a test';
+		$model->other_field = 'this is another field';
+		$model->save();
 
-        $model->name = 'this is a test';
-        $model->other_field = 'this is another field';
-        $model->save();
+		$this->assertEquals('this-is-a-test-this-is-another-field', $model->url);
+	}
 
-        $this->assertEquals('this-is-a-test-this-is-another-field', $model->url);
-    }
+	/** @test */
+	public function itCanGenerateSlugsFromACallable() {
+		$model = new class extends TestModel {
+			public function getSlugOptions() {
+				return parent::getSlugOptions()->generateSlugsFrom(function (TestModel $model): string {
+					return 'foo-' . str_slug($model->name);
+				});
+			}
+		};
 
-    /** @test */
-    public function it_can_generate_slugs_from_a_callable()
-    {
-        $model = new class extends TestModel
-        {
-            public function getSlugOptions(): SlugOptions
-            {
-                return parent::getSlugOptions()->generateSlugsFrom(function (TestModel $model): string {
-                    return 'foo-' . str_slug($model->name);
-                });
-            }
-        };
+		$model->name = 'this is a test';
+		$model->save();
 
-        $model->name = 'this is a test';
-        $model->save();
+		$this->assertEquals('foo-this-is-a-test', $model->url);
+	}
 
-        $this->assertEquals('foo-this-is-a-test', $model->url);
-    }
+	/** @test */
+	public function itCanGenerateDuplicateSlugs() {
+		foreach (range(1, 10) as $i) {
+			$model = new class extends TestModel {
+				public function getSlugOptions() {
+					return parent::getSlugOptions()->allowDuplicateSlugs();
+				}
+			};
 
-    /** @test */
-    public function it_can_generate_duplicate_slugs()
-    {
-        foreach (range(1, 10) as $i) {
-            $model = new class extends TestModel
-            {
-                public function getSlugOptions(): SlugOptions
-                {
-                    return parent::getSlugOptions()->allowDuplicateSlugs();
-                }
-            };
+			$model->name = 'this is a test';
+			$model->save();
 
-            $model->name = 'this is a test';
-            $model->save();
+			$this->assertEquals('this-is-a-test', $model->url);
+		}
+	}
 
-            $this->assertEquals('this-is-a-test', $model->url);
-        }
-    }
+	/** @test */
+	public function itCanGenerateSlugsWithAMaximumLength() {
+		$model = new class extends TestModel {
+			public function getSlugOptions() {
+				return parent::getSlugOptions()->slugsShouldBeNoLongerThan(5);
+			}
+		};
 
-    /** @test */
-    public function it_can_generate_slugs_with_a_maximum_length()
-    {
-        $model = new class extends TestModel
-        {
-            public function getSlugOptions(): SlugOptions
-            {
-                return parent::getSlugOptions()->slugsShouldBeNoLongerThan(5);
-            }
-        };
+		$model->name = '123456789';
+		$model->save();
 
-        $model->name = '123456789';
-        $model->save();
+		$this->assertEquals('12345', $model->url);
+	}
 
-        $this->assertEquals('12345', $model->url);
-    }
+	/**
+	 * @test
+	 * @dataProvider weirdCharacterProvider
+	 */
+	public function itCanHandleWeirdCharactersWhenGeneratingTheSlug(string $weirdCharacter, string $normalCharacter) {
+		$model = TestModel::create(['name' => $weirdCharacter]);
 
-    /**
-     * @test
-     * @dataProvider weirdCharacterProvider
-     */
-    public function it_can_handle_weird_characters_when_generating_the_slug(string $weirdCharacter, string $normalCharacter)
-    {
-        $model = TestModel::create(['name' => $weirdCharacter]);
+		$this->assertEquals($normalCharacter, $model->url);
+	}
 
-        $this->assertEquals($normalCharacter, $model->url);
-    }
+	public function weirdCharacterProvider() {
+		return [
+			['é', 'e'],
+			['è', 'e'],
+			['à', 'a'],
+			['a€', 'a'],
+			['ß', 'ss'],
+			['a/ ', 'a'],
+		];
+	}
 
-    public function weirdCharacterProvider()
-    {
-        return [
-            ['é', 'e'],
-            ['è', 'e'],
-            ['à', 'a'],
-            ['a€', 'a'],
-            ['ß', 'ss'],
-            ['a/ ', 'a'],
-        ];
-    }
+	/** @test */
+	public function itCanHandleOverwritesWhenUpdatingAModel() {
+		$model = TestModel::create(['name' => 'this is a test']);
 
-    /** @test */
-    public function it_can_handle_overwrites_when_updating_a_model()
-    {
-        $model = TestModel::create(['name' => 'this is a test']);
+		$model->url = 'this-is-an-url';
+		$model->save();
 
-        $model->url = 'this-is-an-url';
-        $model->save();
+		$this->assertEquals('this-is-an-url', $model->url);
+	}
 
-        $this->assertEquals('this-is-an-url', $model->url);
-    }
+	/** @test */
+	public function itCanHandleDuplicatesWhenOverwritingASlug() {
+		$model = TestModel::create(['name' => 'this is a test']);
+		$other_model = TestModel::create(['name' => 'this is an other']);
 
-    /** @test */
-    public function it_can_handle_duplicates_when_overwriting_a_slug()
-    {
-        $model = TestModel::create(['name' => 'this is a test']);
-        $other_model = TestModel::create(['name' => 'this is an other']);
+		$model->url = 'this-is-an-other';
+		$model->save();
 
-        $model->url = 'this-is-an-other';
-        $model->save();
-
-        $this->assertEquals('this-is-an-other-1', $model->url);
-    }
+		$this->assertEquals('this-is-an-other-1', $model->url);
+	}
 }
