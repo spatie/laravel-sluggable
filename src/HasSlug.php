bd4cc1b7ce2a4775f3ae39ea90df7a7813af8dff
marvin-wtt <31454580+marvin-wtt@users.noreply.github.com>
2022-04-04 01:33:28 +0200

    Adds missing return type

--- src/HasSlug.php
+++ src/HasSlug.php
@@ -181,7 +181,7 @@ trait HasSlug
      * Helper function to handle multi-bytes strings if the module mb_substr is present,
      * default to substr otherwise.
      */
-    protected function generateSubstring($slugSourceString)
+    protected function generateSubstring($slugSourceString): string
     {
         if (function_exists('mb_substr')) {
             return mb_substr($slugSourceString, 0, $this->slugOptions->maximumLength);
